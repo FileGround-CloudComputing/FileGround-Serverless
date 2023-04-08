7ae2d8707b3b52fe63c4c99b755a2fbb0ab9da4a
joybully <joybully@khu.ac.kr>
2023-04-08 22:05:23 +0900

    make_trigger1

--- functions/index.js
+++ functions/index.js
@@ -9,23 +9,17 @@ const fs = require('fs');
 // https://firebase.google.com/docs/functions/get-started
 
 // Download file from bucket.
-const bucket = admin.storage().bucket(fileBucket);
-const tempFilePath = path.join(os.tmpdir(), fileName);
+const bucket = admin.storage().bucket('file-ground-images');
+const tempFilePath = path.join(os.tmpdir(), 'numm.txt');
 const metadata = {
   contentType: contentType,
 };
-await bucket.file(filePath).download({destination: tempFilePath});
+await bucket.file('gs://file-ground-images//numm.txt').download({destination: tempFilePath});
 functions.logger.log('Image downloaded locally to', tempFilePath);
 // Generate a thumbnail using ImageMagick.
 await spawn('convert', [tempFilePath, '-thumbnail', '200x200>', tempFilePath]);
 functions.logger.log('Thumbnail created at', tempFilePath);
 // We add a 'thumb_' prefix to thumbnails file name. That's where we'll upload the thumbnail.
-const thumbFileName = `thumb_${fileName}`;
-const thumbFilePath = path.join(path.dirname(filePath), thumbFileName);
-// Uploading the thumbnail.
-await bucket.upload(tempFilePath, {
-  destination: thumbFilePath,
-  metadata: metadata,
-});
+
 // Once the thumbnail has been uploaded delete the local file to free up disk space.
-return fs.unlinkSync(tempFilePath);
+//return fs.unlinkSync(tempFilePath);
