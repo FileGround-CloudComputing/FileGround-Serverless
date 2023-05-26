1a7a0a084d869cc55e1c9a32a9a48bd52bf87e2a
sangeun <sangeun99@khu.ac.kr>
2023-05-26 23:44:38 +0900

    feat: 썸네일 저장 시 DB 갱신

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,25 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const path = require('path');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+const THUMB_PREFIX = 'thumb_';
+
+exports.setThumbnailRTDB = functions.storage
+  .bucket("file-ground-images")
+  .object()
+  .onFinalize(async (object) => {
+    const thumbnailPath = object.name; // Thumbnail path in the bucket.
+    const thumbnailDir = path.dirname(thumbnailPath); // ex)grounds/0/...
+    const thumbnailName = path.basename(thumbnailPath); // Get the Thumbnail name.
+
+    // Exit if the image is not a thumbnail
+    if (!thumbnailName.startsWith(THUMB_PREFIX)) {
+      console.log('This is not a Thumbnail.');
+      return null;
+    }
+
+    const fileName = thumbnailName.replace(THUMB_PREFIX, '');
+
+    return admin.database().ref().child('/${thumbnailDir}/photos/${fileName}').update({ thumbnail: thumbnailPath })
+  });
