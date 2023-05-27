e25637dc73fe2b194ec2d735e36b99c36faac713
sunwu <why@gmail.com>
2023-05-28 04:00:45 +0900

    dfd

--- functions/index.js
+++ functions/index.js
@@ -34,6 +34,27 @@ let fileIdNum;
  * ImageMagick.
  */
 // [START generateThumbnailTrigger]
+
+exports.backupDeletedArticle = functions.database.instance('file-ground-grounds').ref('/grounds/{rowId}')
+    .onDelete((snapshot, context) => {
+      const bucket = admin.storage().bucket();
+      const rowId = context.params.rowId; // Store the value of rowId in a variable
+
+      const deleteOptions = {
+        prefix: `grounds/${rowId}/`
+      };
+      // Perform additional permission checks if necessary
+      // For example, you can compare the 'uid' to the authenticated user's ID
+
+      return bucket.deleteFiles(deleteOptions)
+        .then(() => {
+          console.log(`All the Firebase Storage files in grounds/${rowId}/ have been deleted`);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+
 exports.generateThumbnail = functions.storage
   .bucket("file-ground-images")
   .object()
